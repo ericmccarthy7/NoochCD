2f7cf2f6254c44c5e08f15274030d39046c60690
Eric McCarthy <ericmccarthy7@gmail.com>
2021-12-31 00:51:18 -0500

    working!

--- NoochCD.lua
+++ NoochCD.lua
@@ -1,3 +1,11 @@
+saltShakerText = "WoW+Cooldown%3A+Salt+Shaker"
+moonclothText = "WoW+Cooldown%3A+Mooncloth"
+transmuteText = "WoW+Cooldown%3A+Transmute+Arcanite"
+
+currentTitle = saltShakerText
+currentDate = "20220101T0000%2F20220101T0005"
+
+
 StaticPopupDialogs["NOOCH_COPYPASTA"] = {
     text = "CTRL+C to copy, paste in browser",
     button1 = OKAY,
@@ -6,7 +14,7 @@ StaticPopupDialogs["NOOCH_COPYPASTA"] = {
     hideOnEscape = true,
     hasEditBox = true,
     OnShow = function (self, data)
-        self.editBox:SetText("Some text goes here")
+        self.editBox:SetText("https://www.google.com/calendar/render?action=TEMPLATE&text=" .. currentTitle .. "&dates=" .. currentDate)
         self.editBox:HighlightText()
         self.editBox:SetFocus()
     end,
@@ -15,31 +23,82 @@ StaticPopupDialogs["NOOCH_COPYPASTA"] = {
     end,
 }
 
+StaticPopupDialogs["NOOCH_ERRORDIALOG"] = {
+    text = "Unknown error",
+    button1 = OKAY,
+    timeout = 0,
+    whileDead = true,
+    hideOnEscape = true,
+    OnShow = function (self, data)
+        self.text:SetText("No cooldown / spell doesn't exist.")
+        self.button1:SetFocus()
+    end,
+}
+
 function NoochCooldowns(msg)
     -- TODO: check salt shaker in inventory.
     if(msg == "lw") then
         startTime, duration, enable = GetItemCooldown(15846)
-        print("start: ", startTime)
-        print("duration: ", duration)
-        print("enable: ", enable)
+        if(startTime == 0) then
+            StaticPopup_Show("NOOCH_ERRORDIALOG")
+        else
+            local remaining = GetCooldownLeft(startTime, duration)
+            local from = date("%Y%m%dT%H%M%S", time() + remaining)
+            local to = date("%Y%m%dT%H%M%S", time() + remaining + 300)
+            currentDate = from .. "%2F" .. to
+            currentTitle = saltShakerText
+            StaticPopup_Show("NOOCH_COPYPASTA")
+        end
     elseif(msg == "alch") then
         startTime, duration, enable = GetSpellCooldown(17187)
-        print("usable: ", IsUsableSpell(17187))
-        print("start: ", startTime)
-        print("duration: ", duration)
-        print("enable: ", enable)
+        if(startTime == 0) then
+            StaticPopup_Show("NOOCH_ERRORDIALOG")
+        else
+            local remaining = GetCooldownLeft(startTime, duration)
+            local from = date("%Y%m%dT%H%M%S", time() + remaining)
+            local to = date("%Y%m%dT%H%M%S", time() + remaining + 300)
+            currentDate = from .. "%2F" .. to
+            currentTitle = transmuteText
+            StaticPopup_Show("NOOCH_COPYPASTA")
+        end
     elseif(msg == "cloth") then
         startTime, duration, enable = GetSpellCooldown(18560)
-        print("usable: ", IsUsableSpell(18560))
-        print("start: ", startTime)
-        print("duration: ", duration)
-        print("enable: ", enable)
+        if(startTime == 0) then
+            StaticPopup_Show("NOOCH_ERRORDIALOG")
+        else
+            local remaining = GetCooldownLeft(startTime, duration)
+            local from = date("%Y%m%dT%H%M%S", time() + remaining)
+            local to = date("%Y%m%dT%H%M%S", time() + remaining + 300)
+            currentDate = from .. "%2F" .. to
+            currentTitle = moonclothText
+            StaticPopup_Show("NOOCH_COPYPASTA")
+
+        end
     else
-        StaticPopup_Show("NOOCH_COPYPASTA")
+        StaticPopup_Show("NOOCH_ERRORDIALOG")
     end
 end
 
+function GetCooldownLeft(start, duration)
+    -- https://github.com/Stanzilla/WoWUIBugs/issues/47
+    if start < GetTime() then
+        local cdEndTime = start + duration
+        local cdLeftDuration = cdEndTime - GetTime()
+        
+        return cdLeftDuration
+    end
 
+    local time = time()
+    local startupTime = time - GetTime()
+    -- just a simplification of: ((2^32) - (start * 1000)) / 1000
+    local cdTime = (2 ^ 32) / 1000 - start
+    local cdStartTime = startupTime - cdTime
+    local cdEndTime = cdStartTime + duration
+    local cdLeftDuration = cdEndTime - time
+    
+    return cdLeftDuration
+end
 
 SLASH_NOOCH1 = "/nooch"
+SLASH_NOOCH2 = "/n"
 SlashCmdList["NOOCH"] = NoochCooldowns
